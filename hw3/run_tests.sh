cd7ba4b818a1fa914ef4d168bfb55cd4fbd2b7a0
Tal Rosenseweig <>
2021-12-22 00:36:07 +0200

    fix in test script

--- hw3/run_tests.sh
+++ hw3/run_tests.sh
@@ -5,7 +5,7 @@ for i in {1..4}
 do
    echo "testing test ${i}"
 
-./hw3.out < hw3-tests/t${i}.in > hw3-tests/t${i}.res
+./hw3 < hw3-tests/t${i}.in > hw3-tests/t${i}.res
 
 diff hw3-tests/t${i}.out hw3-tests/t${i}.res
 done
