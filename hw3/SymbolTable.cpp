8224fff0a1d3799fae8fac11d666c896de7cb37d
Tal Rosenseweig <>
2021-12-22 15:47:53 +0200

    added symbol table files

---
+++ hw3/SymbolTable.cpp
@@ -0,0 +1,101 @@
+//
+// Created by Tal Rosenseweig on 22/12/2021.
+//
+
+#include "SymbolTable.h"
+#include "hw3_output.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace output;
+extern int yylineno;
+
+void Symbol::print_symbol() {
+    printID(id, offset, type);
+}
+
+void Function::print_symbol() {
+    printID(get_id(), get_offset(), makeFunctionType(get_type(), argTypes));
+}
+
+void SymbolTable::new_scope() {
+    Scope scope;
+    scopes.push(scope);
+    int new_offset = offsets.top();
+    offsets.push(new_offset);
+}
+
+bool SymbolTable::id_exists(string id){return !(declared_symbols.find(id) == declared_symbols.end());}
+
+bool SymbolTable::add_symbol(string id, string type, bool is_const){
+    if (id_exists(id)){
+        return false;
+    }
+    Symbol* new_symbol = new Symbol(id, type, is_const);
+    declared_symbols.insert({id, new_symbol});
+
+    //add symbol
+    new_symbol->set_offset(offsets.top());
+    scopes.top().push_back(new_symbol);
+    offsets.top()++;
+    return true;
+}
+
+bool SymbolTable::add_function(string id, string type, vector<string> argTypes){
+    if (id_exists(id)){
+        return false;
+    }
+    Function* new_func = new Function(id, type, argTypes);
+
+    last_func_return_type = type;
+    was_main_declared = was_main_declared || new_func->get_is_main();
+
+    declared_symbols.insert({id, new_func});
+    new_func->set_offset(offsets.top());
+    scopes.top().push_back(new_func);
+
+    return true;
+}
+
+string SymbolTable::add_function_args_symbols(vector<string> arg_ids, vector<string> argTypes, vector<bool> arg_consts){
+    if(arg_ids.empty()) return "";
+
+    for(size_t i = 0; i < arg_ids.size() ; i++)
+    {
+        if (id_exists(arg_ids[i])){
+            return arg_ids[i];
+        }
+        Symbol* new_symbol = new Symbol(arg_ids[i], arg_types[i], arg_consts[i]);
+        declared_symbols.insert({arg_ids[i], new_symbol});
+        new_symbol->set_offset(-(i+1));
+        scopes.top().push_back(new_symbol);
+    }
+    return "";
+}
+
+string SymbolTable::get_type_by_id(string id){
+    if (!id_exists(id)) return "ERROR";
+    return declared_symbols[id]->get_type();
+}
+
+bool SymbolTable::get_const_by_id(string id){
+    if (!id_exists(id)) return false;
+    return declared_symbols[id]->get_const();
+}
+
+Function* SymbolTable::get_function_by_id(string id){
+    if(!id_exists(id)) return nullptr;
+    return dynamic_cast<Function*>(declared_symbols[id]);
+}
+
+void SymbolTable::delete_scope() {
+    endScope();
+
+    for(auto& symbol : scopes.top()) {
+        symbol->print_symbol();
+        declared_symbols.erase(symbol->get_id());
+    }
+
+    scopes.pop();
+    offsets.pop();
+}
---
+++ hw3/SymbolTable.h
@@ -0,0 +1,65 @@
+#ifndef HW3_SYMBOLTABLE_H
+#define HW3_SYMBOLTABLE_H
+
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Symbol{
+    string id;
+    string type;
+    bool is_const;
+    int offset = 0;
+
+public:
+    Symbol(string id, string type, bool is_const=false) : id(id), type(type), is_const(is_const) {}
+    string& get_id() {return id;}
+    string& get_type() {return type;}
+    bool& get_const() {return is_const;}
+    int get_offset() {return offset;}
+    void set_offset(int offset_num) {offset = offset_num;}
+    virtual void print_symbol();
+    virtual ~Symbol() = default;
+};
+
+class Function : public Symbol{
+    vector<string> argTypes;
+    bool is_main = false;
+
+public:
+    Function(string id, string type, vector<string> argTypes, bool is_const) : Symbol(id, type, is_const), argTypes(argTypes) {
+        is_main = (id == "main") && (type == "VOID") && (argTypes.empty());
+    }
+    bool& get_is_main(){return is_main;}
+    void print_symbol() override;
+    ~Function() = default;
+};
+
+typedef vector<Symbol*> Scope;
+
+class SymbolTable {
+    stack<Scope> scopes;
+    stack<int> offsets;
+    unordered_map<string, Symbol*> declared_symbols;
+
+    string last_func_return_type;
+    bool was_main_declared = false;
+
+    SymbolTable();
+
+    void new_scope();
+    bool id_exists(string id);
+    bool add_symbol(string id, string type, bool is_const=false);
+    bool add_function(string id, string ret_type, vector<string> argTypes);
+    string add_function_args_symbols(vector<string> arg_ids, vector<string> argTypes, vector<bool> arg_consts);
+    string get_type_by_id(string id);
+    bool get_const_by_id(string id);
+    Function* get_function_by_id(string id);
+    void delete_scope();
+    ~SymbolsTableManager() = default;
+};
+
+#endif //HW3_SYMBOLTABLE_H
